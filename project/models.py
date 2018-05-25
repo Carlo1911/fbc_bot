d6f376966f5352bf8c82737f0700267ae8e94543
Carlo1911 <carlo.alva@pucp.pe>
2018-05-25 01:36:19 -0500

    New reports

--- project/models.py
+++ project/models.py
@@ -1,11 +1,31 @@
 from project import db
+from datetime import datetime
+
+
+favorites = db.Table('favorites',
+                     db.Column('users_recipient_id', db.String(18),
+                               db.ForeignKey('users.recipient_id')),
+                     db.Column('songs_track_id', db.String(18),
+                               db.ForeignKey('songs.track_id')),
+                     db.UniqueConstraint(
+                         'users_recipient_id', 'songs_track_id',
+                         name='UC_user_id_song_id')
+                     )
 
 
 class User(db.Model):
-    __tablename__ = 'fb_users'
+    __tablename__ = 'users'
     recipient_id = db.Column(db.String(18), primary_key=True)
     first_name = db.Column(db.String(64))
     last_name = db.Column(db.String(120))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    last_connection = db.Column(
+        db.DateTime, default=datetime.utcnow)
+    songs_list = db.relationship(
+        'Song',
+        secondary=favorites,
+        backref=db.backref('songs_list', lazy='dynamic')
+    )
 
     def __init__(self, recipient_id, first_name, last_name):
         self.recipient_id = recipient_id
@@ -14,3 +34,24 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.recipient_id)
+
+
+class Song(db.Model):
+    __tablename__ = 'songs'
+    track_id = db.Column(db.String(18), primary_key=True)
+    track_name = db.Column(db.String(120))
+    artist_name = db.Column(db.String(120))
+    searched_times = db.Column(db.Integer)
+    users_list = db.relationship(
+        'User',
+        secondary=favorites,
+        backref=db.backref('users_list', lazy='dynamic'))
+
+    def __init__(self, track_id, track_name, artist_name):
+        self.track_id = track_id
+        self.track_name = track_name
+        self.artist_name = artist_name
+        self.searched_times = 1
+
+    def __repr__(self):
+        return '<Song %r>' % (self.track_name)
--- project/views.py
+++ project/views.py
@@ -2,15 +2,16 @@ import os
 import requests
 import random
 import json
+from datetime import datetime, date
 from flask import Flask, request, Blueprint
 
 from project import db, bot
-from project.models import User
+from project.models import User, Song
 
 
 project_blueprint = Blueprint(
     'home', __name__,
-)   # pragma: no cover
+)
 
 
 @project_blueprint.route('/', methods=['GET'])
@@ -19,13 +20,11 @@ def verify():
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
-
     return "Ok", 200
 
 
 @project_blueprint.route('/', methods=['POST'])
 def webhook():
-
     output = request.get_json()
     print(output)
     for event in output['entry']:
@@ -33,44 +32,93 @@ def webhook():
         for message in messaging:
             if message.get('postback'):
                 recipient_id = message['sender']['id']
-                track_id = message['postback'].get('payload')
-                add_music(recipient_id, track_id)
-                # add_music(recipient_id, track_id)
+                print(message['postback'].get('title'))
+                if 'Agregar canción' == message['postback'].get('title'):
+                    track_id = message['postback'].get('payload')
+                    add_music(recipient_id, track_id)
+                    send_message(recipient_id, 'Canción agregada')
+                if 'Lista de favoritos' == message['postback'].get('title'):
+                    favorite_songs = get_favorite_songs(recipient_id)
+                    print(favorite_songs)
+                    send_message(recipient_id, favorite_songs)
+                if 'Mostrar usuarios' == message['postback'].get('title'):
+                    total_users = get_total_users()
+                    send_message(recipient_id, total_users)
+                if 'Chats hoy' == message['postback'].get('title'):
+                    chats_today = get_total_chats()
+                    send_message(recipient_id, chats_today)
             if message.get('message'):
                 recipient_id = message['sender']['id']
                 mensaje = message['message'].get('text')
                 if message['message'].get('text'):
-                    if mensaje == 'Hola':
-                        send_message(recipient_id, 'Hola')
-                        print(bot.get_user_info(recipient_id))
-                    else:
-                        if 'buscar:' in mensaje:
-                            results = get_results(
-                                mensaje.split('buscar:')[1].strip())
-                        response_sent_text = get_message()
-                        send_message(recipient_id, response_sent_text)
+                    if 'buscar:' in mensaje:
+                        results = get_results(
+                            mensaje.split('buscar:')[1].strip())
                         send_generic_message(recipient_id, results[:3])
-                # if user sends us a GIF, photo,video, or any other non-text item
-                if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
-                    send_message(recipient_id, response_sent_nontext)
+                    if 'Reportes:' in mensaje:
+                        reports = get_reports()
+                        send_generic_message(recipient_id, reports)
     return "Message Processed"
 
 
 def add_music(recipient_id, track_id):
     user_info = bot.get_user_info(recipient_id)
     user = User.query.filter_by(recipient_id=recipient_id).first()
-    if user:
-        # agregar música
-        pass
-    else:
+    # check if user exists in bd
+    if not user:
         user = User(
             recipient_id=recipient_id,
             first_name=user_info['first_name'],
             last_name=user_info['last_name'])
         db.session.add(user)
+    else:
+        # Update user's last conection
+        user.last_connection = datetime.utcnow()
+    db.session.commit()
+    parameters = {
+        'apikey': '2df63ad0b5eb5f9d024490851cb059a7',
+        'track_id': track_id,
+    }
+    api_url = 'http://api.musixmatch.com/ws/1.1/track.get'
+    response = requests.get(api_url, params=parameters)
+    results = None
+    if response.status_code == 200:
+        results = json.loads(response.content.decode('utf-8'))
+        track = results['message']['body']['track']
+        song = Song.query.filter_by(track_id=track_id).first()
+        # check if song exist in bd
+        if not song:
+            song = Song(track_id=track_id,
+                        track_name=track['track_name'],
+                        artist_name=track['artist_name'])
+            db.session.add(song)
+        favorite_song = User.query.filter(
+            User.songs_list.any(track_id=track_id)).first()
+        # check if user has song as favorite
+        if favorite_song:
+            favorite_song.searched_times += 1
+        else:
+            user.songs_list.append(song)
         db.session.commit()
-    return "success"
+
+
+def get_total_chats():
+    start = date.today()
+    total_chats = User.query.filter(User.last_connection >= start).count()
+    return 'Total chats de hoy: {}'.format(total_chats)
+
+
+def get_total_users():
+    total_users = User.query.count()
+    return 'Total de usuarios: {}'.format(total_users)
+
+
+def get_favorite_songs(recipient_id):
+    user = User.query.filter_by(recipient_id=recipient_id).first()
+    favorite_songs = ''
+    for song in user.songs_list:
+        favorite_songs += '{} - {}\n'.format(song.track_name, song.artist_name)
+    return favorite_songs
 
 
 def get_results(searched_word):
@@ -85,7 +133,6 @@ def get_results(searched_word):
     if response.status_code == 200:
         results = json.loads(response.content.decode('utf-8'))
         track_list = results['message']['body']['track_list']
-        print("Canción - Artista")
         songs = []
         for element in track_list:
             song_info = {
@@ -102,36 +149,39 @@ def get_results(searched_word):
     return results
 
 
+def get_reports():
+    reports = [
+        {
+            "title": 'Canciones favoritas',
+            "buttons": [{
+                "type": "postback",
+                "title": "Lista de favoritos",
+                "payload": "True",
+            }],
+        },
+        {
+            "title": 'Total de usuarios',
+            "buttons": [{
+                "type": "postback",
+                "title": "Mostrar usuarios",
+                "payload": "True",
+            }],
+        },
+        {
+            "title": 'Chats de hoy',
+            "buttons": [{
+                "type": "postback",
+                "title": "Chats hoy",
+                "payload": "True",
+            }]
+        }
+    ]
+    return reports
+
+
 def send_generic_message(recipient_id, elements):
     bot.send_generic_message(recipient_id, elements)
-    return "success"
-
-
-def get_message():
-    sample_responses = ["You are stunning!", "We're proud of you.",
-                        "Keep on being you!", "We're greatful to know you :)"]
-    # return selected item to the user
-    return random.choice(sample_responses)
 
 
 def send_message(recipient_id, response):
-    # sends user the text message provided via input response parameter
     bot.send_text_message(recipient_id, response)
-    return "success"
-
-
-def send_button_message(recipient_id, response):
-    buttons = [
-        {
-            "type": "web_url",
-            "url": "https://www.messenger.com",
-            "title": "Visit Messenger"
-        },
-        {
-            "type": "postback",
-            "title": "Agregar canción: One kiss",
-            "payload": "track_id: 12313131321"
-        }
-    ]
-    bot.send_button_message(recipient_id, response, buttons)
-    return "success"
